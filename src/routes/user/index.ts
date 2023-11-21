150de82e02993f13e123a19ddaa3865713521cbd
Jan Czibula <jan.czibula@gmail.com>
2023-11-21 11:57:18 +0100

    simplify user router

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -2,30 +2,16 @@ import { Router } from 'express';
 
 const router = Router();
 
-import CreateRoute from './routes/create';
-import ListRoute from './routes/list';
-import ReadRoute from './routes/read';
+import createRoute from './routes/create';
+import listRoute from './routes/list';
+import readRoute from './routes/read';
 import removeRoute from './routes/remove';
 import updateRoute from './routes/update';
 
-router.route('/user/list').get((req, res) => {
-	return ListRoute(req, res);
-});
-
-router.route('/user/read/:userId').get((req, res) => {
-	return ReadRoute(req, res);
-});
-
-router.route('/user/create').put((req, res) => {
-	return CreateRoute(req, res);
-});
-
-router.route('/user/update').post((req, res) => {
-	return updateRoute(req, res);
-});
-
-router.route('/user/remove/:userId').delete((req, res) => {
-	return removeRoute(req, res);
-});
+router.route('/user/list').get(listRoute);
+router.route('/user/read/:userId').get(readRoute);
+router.route('/user/create').put(createRoute);
+router.route('/user/update').post(updateRoute);
+router.route('/user/remove/:userId').delete(removeRoute);
 
 export default router;
